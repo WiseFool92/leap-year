52026d8c5e7a1b3ff526d8c54599e0fc8d73fd61
josh <jhell85@gmail.com>
2020-04-01 11:12:21 -0700

    finished leap year function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 // Back End
+
 function evaluateYear(year) {
-	if (year % 100 === 0) {
-		return false;
-	}
-	else if (year % 4 === 0) {
+	if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
 		return true;
-  } else {
+	} else {
 		return false;
 	}
 };
