7399f96e80bd1eec8df31a5c5592635f4afe7709
josh <jhell85@gmail.com>
2020-04-01 16:28:27 -0700

    add -yay to the end of words that start wit ha vowel

--- js/scripts.js
+++ js/scripts.js
@@ -11,23 +11,22 @@ function evaluateYear(year) {
 // pig latin
 function createPiglatin(phrase) {
   var phraseArray = phrase.split(" ");
-  // console.log(phraseArray);
   for (let index = 0; index < phraseArray.length; index++) {
     var word = phraseArray[index];
-    const notChar = word.match(/^[A-Za-z]+$/g);
-    if (notChar === null) {
-    } else if(word.length === 1) {    
-      const notaVowel = word.match(/[aeiouAEIOU]/g)
-      //console.log(notaVowel);
-      if(notaVowel === null){ 
-      } else{
-        word += '-yay'
-        phraseArray[index] = word 
-      }
-    } else {
-      console.log(notChar);
+    var letterArray = word.split("");
 
-      var letterArray = word.split("");
+    const charCheck = word.match(/^[A-Za-z]+$/g);
+    if (charCheck === null) {
+    } else if (word.length === 1) {
+      const vowelCheck = word.match(/[aeiouAEIOU]/g);
+      if (vowelCheck) {
+        word += "-yay";
+        phraseArray[index] = word;
+      }
+    } else if (word[0].match(/[aeiouAEIOU]/g)) {
+      word += "-yay";
+      phraseArray[index] = word;
+
       for (let index = 0; index < letterArray.length; index++) {
         const letter = letterArray[index];
 
