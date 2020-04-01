fbc880ef88c8f16eca199d696db29df3d6744d2e
josh <jhell85@gmail.com>
2020-04-01 11:56:50 -0700

    refactored code for leap year

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
 // Back End
 
 function evaluateYear(year) {
-	if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-		return true;
-	} else {
-		return false;
-	}
-};
-	
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //Front End
 
 $(document).ready(function() {
-	console.log('JavaScript is working');
+  console.log("JavaScript is working");
 
-	//on click function
-	$('form').submit(function(event) {
-		event.preventDefault();
+  //on click function
+  $("form").submit(function(event) {
+    event.preventDefault();
     var year = $("#input-1").val();
-    console.log(evaluateYear(year));
-	
-    $(".output").text(year)
-   
-	});
+    $(".output1").text(`the year ${year} is ${evaluateYear(year)} and is a leap year`);
+
+    
+  });
 });
